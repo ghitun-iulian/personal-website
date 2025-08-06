0dde9c456291a0c9101eab1b7ac311f6d6ff1eac
ghitun-iulian <ghitun.iulian@yahoo.com>
2025-08-06 13:06:06 +0300

    updated build setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   template: `<router-outlet></router-outlet> `,
 })
 export class App {
-  protected title = 'cv';
+  protected title = 'ghitun-iulian';
 }
